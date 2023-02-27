3a084080a8fadaa26ad98a1bde886dcdd75bfe05
neilCao <1935621364@qq.com>
2023-02-27 15:48:49 +0800

    二分查找模板代码

--- src/array/doublepoint/leftright/binarysearch/example/BinarySearchExample.java
+++ src/array/doublepoint/leftright/binarysearch/example/BinarySearchExample.java
@@ -9,6 +9,21 @@ package array.doublepoint.leftright.binarysearch.example;
  */
 public class BinarySearchExample {
 
-
+    public int binarySearcheExample(int[] nums, int target) {
+        int left, right;
+        left = 0;
+        right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + right / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid + 1;
+            }
+        }
+        return -1;
+    }
 
 }
